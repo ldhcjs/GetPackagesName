d43d637a7f43282934de708d0759196a70ff372c
Tony Lee <ldhcjs@gmail.com>
2019-05-22 15:27:34 +0900

    Added to execute Accessbility Setting

--- app/src/main/java/com/example/packagesname/MainActivity.java
+++ app/src/main/java/com/example/packagesname/MainActivity.java
@@ -3,14 +3,27 @@ package com.example.packagesname;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnSetting = (Button) findViewById(R.id.btn_setting);
 
+        mBtnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS),
+                        1000);
+            }
+        });
         startService(new Intent(this, GetPackagesName.class));
     }
 }
